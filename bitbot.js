3986c7523a37a56ccd5fa1908cf77fdc524cb403
Drewman5000 <andrewrhyland@gmail.com>
2020-06-26 12:30:17 -0700

    Update bitbot.js
    Added too much stuff.

--- bitbot.js
+++ bitbot.js
@@ -16,17 +16,37 @@ bitbot.on('ready', () => {
   console.info(`Logged in as ${bitbot.user.tag}!`);
 });
 
+const coinFlip = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
 bitbot.on('message', msg => {
   const args = msg.content.split(/ +/);
   const command = args.shift().toLowerCase();
   console.info(`Called command: ${command}`);
 
-  if (!bitbot.commands.has(command)) return;
+  if (message.substring(0, 1) == '!') {
+    const pieces = message.substring(1).split(' ');
+    const cmd = pieces[0];
 
-  try {
-    bitbot.commands.get(command).execute(msg, args);
-  } catch (error) {
-    console.error(error);
-    msg.reply('There was an error trying to execute that command!');
+    pieces = pieces.splice(1);
+    switch(cmd) {
+      case 'ping':
+
+      if (!bitbot.commands.has(command)) return;
+
+      try {
+        bitbot.commands.get(command).execute(msg, args);
+      } catch (error) {
+        console.error(error);
+        msg.reply('There was an error trying to execute that command!');
+      };
+      break;
+      case 'bitbot':
+        let respond = (coinFlip(0, 1)) ? "Yes." : "No.";
+        msg.channel.send(respond);
+        break;
+    }
   }
 });
