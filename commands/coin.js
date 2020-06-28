a5346f577b80d4a9caaf496514cc4e442aef1003
Drewman5000 <andrewrhyland@gmail.com>
2020-06-28 16:22:02 -0700

    add coin flip command
    Added coin flip command

---
+++ commands/coin.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'coin',
+	description: 'Flips a coin.',
+	execute(msg, args) {
+		const coinFlip = (min, max) => {
+  			return Math.floor(Math.random() * (max - min + 1) ) + min;
+		}
+		let reply = "";
+		let result = coinFlip(0, 1);
+
+		reply = (result) ? "Heads" : "Tails";
+
+		msg.channel.send(reply);
+	},
+};
